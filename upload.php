87f56b3988cf6670588236d7f522219906fee987
MichaelGonch <michaelgoncharov@hotmail.com>
2020-06-04 00:27:13 -0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -30,7 +30,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+&& $imageFileType != "gif" && $imageFileType != "png" ) {
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
