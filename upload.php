57948aaeaa3b5dec5ff70801f2d0a46fcfe538c2
MichaelGonch <michaelgoncharov@hotmail.com>
2020-06-04 01:00:18 -0700

    Update and rename upload.html to upload.php

---
+++ upload.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file">
+	<button type="submit" name="submit">UPLOAD</button>
+</form>
+</body>
+</html>
